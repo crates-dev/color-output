e9d2cbba7f1cfc9f90998d2a8fd0138731964643
尤雨东 <root@ltpp.vip>
2025-02-09 21:39:12 +0800

    n

--- sh/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-git init;
-git config core.ignorecase false;
-git remote add gitee git@gitee.com:ltpp-universe/color-output.git;
-git remote add origin git@github.com:ltpp-universe/color-output.git;
